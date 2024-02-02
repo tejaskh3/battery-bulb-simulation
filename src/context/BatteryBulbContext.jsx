b101e12b6d538dfa671716576df49d8728eebcff
tejaskh3 <tejaskh3@gmail.com>
2024-02-02 13:31:29 +0530

    fix:fixed remove bulb controller

--- src/context/BatteryBulbContext.jsx
+++ src/context/BatteryBulbContext.jsx
@@ -8,7 +8,7 @@ export const BatterBulbProvider = ({ children }) => {
   const isBatteryLeft = numberOfBattery?.some(
     (battery) => battery.unitsLeft > 0
   );
-
+  console.log(numberOfBulb);
   // bulb controllers
   const addBulb = () => {
     setNumberOfBulb((prev) => prev + 1);
@@ -18,9 +18,7 @@ export const BatterBulbProvider = ({ children }) => {
       alert("Please provider at least one bulb to remove.");
       return;
     }
-    setNumberOfBulb((prev) => {
-      prev - 1;
-    });
+    setNumberOfBulb((prev) => prev -  1);
   };
 
   // batteryControllers
